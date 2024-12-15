996008f4ad89d051d7f054d4d4d6d0cab251427c
Andreas Casen <andreas.casenarg@gmail.com>
2024-12-15 15:02:56 +0100

    feat: add react router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
+
 import './index.css'
-import App from './App.tsx'
+import Index from './pages/index.tsx'
+import PageLayout from './components/PageLayout.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<BrowserRouter>
+			<PageLayout>
+				<Routes>
+					<Route path="/" element={<Index />} />
+				</Routes>
+			</PageLayout>
+		</BrowserRouter>
+	</StrictMode>
 )
