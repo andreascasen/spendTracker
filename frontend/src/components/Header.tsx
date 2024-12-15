8befb935546efd92f31b26677e6f3cd16ad85a3a
Andreas Casen <andreas.casenarg@gmail.com>
2024-12-15 19:02:50 +0100

    feat: style header title to match app color schema

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,9 @@
 export default function Header() {
 	return (
 		<header className="bg-indigo-950 shadow-lg text-slate-200 py-6 px-4">
-			<h1 className="text-center font-bold text-xl">Spend Tracker</h1>
+			<h1 className="text-center text-cyan-500 font-semibold tracking-widest text-xl">
+				SpendTracker
+			</h1>
 		</header>
 	)
 }
